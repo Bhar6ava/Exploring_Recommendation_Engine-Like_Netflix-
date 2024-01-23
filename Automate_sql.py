289d338120d1ba649f305600bc82bd8be9b8618c
D3va-Bhar6ava <146784367+D3va-Bhar6ava@users.noreply.github.com>
2024-01-23 11:29:56 -0500

    Update Automate_sql.py

--- Automate_sql.py
+++ Automate_sql.py
@@ -1,7 +1,8 @@
 import psycopg2
 from psycopg2 import sql
+import os
 
-# Database connection settings
+
 db_settings = {
     "dbname": "capstone_netflix",
     "user": "postgres",
@@ -11,7 +12,7 @@ db_settings = {
 
 def create_database():
     conn = psycopg2.connect(**db_settings)
-    conn.autocommit = True  # Enable autocommit for creating databases
+    conn.autocommit = True  
     cursor = conn.cursor()
     cursor.execute("CREATE DATABASE capstone_netflix")
     cursor.close()
@@ -20,7 +21,7 @@ def create_database():
 def create_table():
     conn = psycopg2.connect(**db_settings)
     cursor = conn.cursor()
-    
+    '''
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS movies (
             id SERIAL PRIMARY KEY,
@@ -31,16 +32,27 @@ def create_table():
             
         )
     """)
-    
+    '''
     '''
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS user_auth (
             id SERIAL PRIMARY KEY,
             username VARCHAR(255),
-            password VARCHAR(255)
+            password VARCHAR(255),
+            preference_id INTEGER,
+            FOREIGN KEY (preference_id) REFERENCES preference_matrix(user_id)
         )               
     """)
     '''
+
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS recommendations (
+        user_id INTEGER,
+        movies TEXT
+    )
+    """)
+    
 
     conn.commit()
     cursor.close()
@@ -49,14 +61,14 @@ def create_table():
 def drop_table():
     conn = psycopg2.connect(**db_settings)
     cursor = conn.cursor()
-    cursor.execute("DROP TABLE IF EXISTS movies")
+    cursor.execute("DROP TABLE IF EXISTS preference_matrix")
     conn.commit()
     cursor.close()
     conn.close()
 
 def drop_database():
     conn = psycopg2.connect(**db_settings)
-    conn.autocommit = True  # Enable autocommit for dropping databases
+    conn.autocommit = True  
     cursor = conn.cursor()
     cursor.execute(sql.SQL("DROP DATABASE IF EXISTS {}").format(
         sql.Identifier("mydatabase")
@@ -86,10 +98,29 @@ def list_tables():
     conn.close()
     return tables
 
-# Example usage:
+
+def preference_matrix():
+    
+    movie_names = [os.path.splitext(filename)[0] for filename in os.listdir('static/thumbnails')]
+
+    
+    query = "CREATE TABLE preference_matrix (user_id SERIAL PRIMARY KEY, "
+    query += ", ".join(f'"{name}" INTEGER' for name in movie_names)
+    query += ")"
+
+    conn = psycopg2.connect(**db_settings)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
+
+
 if __name__ == "__main__":
     #create_database()
     create_table()
+    #preference_matrix()
     #drop_table()
     #drop_database()
     #print(list_databases())
