cba649d46b088bcca61dc0968bf188f185dd04ee
D3va-Bhar6ava <146784367+D3va-Bhar6ava@users.noreply.github.com>
2024-01-23 11:25:40 -0500

    Update user_auth.py

--- user_auth.py
+++ user_auth.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-import psycopg2
+import psycopg2, requests
 
 #app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static')
 
-# Configure your database settings here
+
 db_settings = {
     "dbname": "capstone_netflix",
     "user": "postgres",
@@ -12,33 +12,49 @@ db_settings = {
     "host": "localhost"
 }
 
-# Define a secret key for session management
+
 app.secret_key = "your_secret_key"
 
 
+add_recommendations = 'yes'
+
+user_recom = ''
 
 
 
 
-# Define a function to establish a database connection
 def connect_to_database():
     return psycopg2.connect(**db_settings)
 
-# Define routes and views
 
-# Login page
+
+
+
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
+    global user_recom
+
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        # Verify user credentials in the database
+        
         if verify_user(username, password):
             session["username"] = username
             
             #return redirect(url_for("movies"))
             #return render_template("movies.html")
-            return redirect(url_for("movies"))
+            #return redirect(url_for("movies"))
+
+            if add_recommendations == 'yes':
+                data = {"user_id":username}
+                user_recom = data.get("user_id")
+                return redirect(url_for("movies_recommend"))
+
+            else:
+                return redirect(url_for("movies"))
             #return "success"
         else:
             return render_template("login.html",error = "User does not exist. Please create an account")
@@ -48,13 +64,13 @@ def login():
 
     return render_template("login.html")
 
-# Create an account page
+
 @app.route("/create_account", methods=["GET", "POST"])
 def create_account():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        # Add user to the database
+        
         if create_user(username, password):
             #return redirect(url_for("login"))
             return "success"
@@ -63,19 +79,19 @@ def create_account():
 
     return render_template("create_account.html")
 
-# Movie content page (requires user authentication)
-# Function to fetch movie data
+
+
 def fetch_movie_data():
     conn = psycopg2.connect(**db_settings)
     cursor = conn.cursor()
 
-    # Execute a SQL query to fetch movie data
+    
     cursor.execute("SELECT name, description, genre, thumbnail_path FROM movies")
 
-    # Fetch all rows from the result set
+    
     movie_data = cursor.fetchall()
 
-    # Create a dictionary to organize movies by genre
+    
     organized_movies = {}
     for movie in movie_data:
         name, description, genre, thumbnail_url = movie
@@ -84,30 +100,127 @@ def fetch_movie_data():
         if len(organized_movies[genre]) < 7: 
             organized_movies[genre].append((name, description, genre, thumbnail_url))
 
-    # Close the database connection
+    
     conn.close()
 
     return organized_movies
 
+def recommended_movie_info(movie_recom):
+    conn = psycopg2.connect(**db_settings)
+    cursor = conn.cursor()
+    print("inside")
+    print(movie_recom)
 
-@app.route("/movies")
-def movies(): 
-    # Fetch movie data from the database
-    movie_data = fetch_movie_data()
+    movie_recom_list = []
 
-    # Render the HTML template and pass the movie data
-    return render_template("movies12.html", movie_data=movie_data)
+    for i in movie_recom:
+        print(i[0])
+        cursor.execute("SELECT name, description, genre, thumbnail_path FROM movies WHERE name = %s", (i[0],))
+        movie_data = cursor.fetchall()  
+        if movie_data:
+            movie_recom_list.append(movie_data[0])
 
+    return movie_recom_list
 
-
-# REST API endpoint to fetch movie details by title
-@app.route("/movie/<string:name>", methods=["GET"])
-def get_movie_details(name):
-    # Connect to the PostgreSQL database
+@app.route("/movies/recommend", methods=['GET'])
+def movies_recommend():
+    
     conn = psycopg2.connect(**db_settings)
     cursor = conn.cursor()
 
-    # Query the database for movie details based on the title
+    cursor.execute("SELECT preference_id FROM user_auth WHERE username = %s", (user_recom,))
+    preference_id = cursor.fetchone()[0]
+
+    cursor.execute("SELECT movies FROM recommendations WHERE user_id = %s", (preference_id,))
+    movie_recom = cursor.fetchall()
+
+    movie_recom_list = recommended_movie_info(movie_recom[-7:])
+    #print(movie_recom_list)
+
+    movie_data = fetch_movie_data()
+
+    return render_template("movies20.html", movie_data=movie_data, movie_recom=movie_recom_list)
+
+@app.route("/movies",methods=['GET'])
+def movies(): 
+
+    movie_data = fetch_movie_data()
+
+    
+    return render_template("movies19.html", movie_data=movie_data)
+
+
+
+@app.route("/track_click", methods=["POST"])
+def track_click():
+    try:
+
+        if request.method == 'OPTIONS':
+            
+            response = jsonify()
+            response.headers['Access-Control-Allow-Methods'] = 'POST'
+            return response
+
+        
+        data = request.get_json()
+        print(data)
+        action = data.get("buttonClicked")
+        movie_title = data.get("movieTitle")
+        print(movie_title)
+        username = data.get("username")  
+
+        
+        value = 0
+        if action == "More":
+            value = 1
+        elif action == "Play Trailer":
+            value = 2
+        elif action == "Watched":
+            value = 3
+        
+        conn = psycopg2.connect(**db_settings)
+        
+        cursor = conn.cursor()
+
+        
+        cursor.execute("SELECT preference_id FROM user_auth WHERE username = %s", (username,))
+        preference_id = cursor.fetchone()[0]
+        print(preference_id)
+
+        
+        cursor.execute(f"SELECT \"{movie_title.replace(' ', '_')}\" FROM preference_matrix WHERE user_id = %s", (preference_id,))
+        current_value = cursor.fetchone()[0]
+        print(current_value)
+
+        if current_value == None:
+            current_value = 0
+        
+        if value > current_value:
+            print("inside")
+            
+            cursor.execute(f"UPDATE preference_matrix SET \"{movie_title.replace(' ', '_')}\" = %s WHERE user_id = %s", (value, preference_id))
+            
+            conn.commit()
+
+        
+        cursor.close()
+        conn.close()
+
+        return jsonify(success=True), 200
+        
+
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+
+
+@app.route("/movie/<string:name>", methods=["GET"])
+def get_movie_details(name):
+    
+    conn = psycopg2.connect(**db_settings)
+    cursor = conn.cursor()
+
+    
     cursor.execute(
         "SELECT description FROM movies WHERE name = %s", (name,)
     )
@@ -115,7 +228,7 @@ def get_movie_details(name):
     print("desc")
     print(movie_details)
     print(type(movie_details))
-    # Close the database connection
+    
     cursor.close()
     conn.close()
 
@@ -124,7 +237,7 @@ def get_movie_details(name):
     
 
 
-# Function to verify user credentials
+
 def verify_user(username, password):
     conn = connect_to_database()
     cursor = conn.cursor()
@@ -133,14 +246,49 @@ def verify_user(username, password):
     conn.close()
     return user is not None
 
-# Function to create a new user account
+
 def create_user(username, password):
     conn = connect_to_database()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO user_auth (username, password) VALUES (%s, %s)", (username, password))
+
+    
+    cursor.execute("INSERT INTO preference_matrix DEFAULT VALUES RETURNING user_id")
+    preference_id = cursor.fetchone()[0]
+
+    
+    cursor.execute("INSERT INTO user_auth (username, password, preference_id) VALUES (%s, %s, %s)", (username, password, preference_id))
+
+
     conn.commit()
     conn.close()
     return True
+
+
+
+@app.route("/movie/recommend/", methods=["POST"])
+def recom_movies():
+
+    conn = psycopg2.connect(**db_settings)
+    cursor = conn.cursor()
+
+    data = request.get_json()
+    print(data)
+
+    user_id = data.get('user_id')
+    recom_movies = data.get('recommended_movie_names')
+    print(user_id)
+    print(recom_movies)
+
+
+    for movie in recom_movies:
+        cursor.execute('''
+            INSERT INTO recommendations (user_id, movies)
+            VALUES (%s, %s)
+        ''', (user_id, movie))
+
+    conn.commit()
+    return jsonify({"message": "Recommendations inserted successfully."})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
